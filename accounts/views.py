742a1e80eb7d25ee5115b1cd3bd550ac303f94ff
mohsenbarari <mmchavoshi235@gmail.com>
2023-09-16 16:27:32 +0100

    add register form,url,view and html page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.views import View
+from django.contrib.auth.models import User
+from .forms import UserRegisterForm
 
-# Create your views here.
+
+class UserRgisterView(View):
+    form_class = UserRegisterForm
+    template_page = "accounts/register.html"
+
+    def get(self,request):
+        form = self.form_class
+        return render(request,self.template_page,{
+            "form":form
+        })
+    
+    def post(self,request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd["username"],cd["email"],cd["password"])
+            messages.success(request,f"Hi {cd['username']}! you'r registered","success")
+            return redirect("home:index")
+        else:
+            return render(request,self.template_page,{
+            "form":form})
+
+
