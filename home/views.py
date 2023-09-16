b1413c345af36778aa671abb8ff73afbcc00b263
mohsenbarari <mmchavoshi235@gmail.com>
2023-09-16 11:31:34 +0100

    add test view in home app

--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 from django.views import View
 
 
 class IndexView(View):
     def get(self,request):
-        return HttpResponse("Hi there, welcome to index page")
+        contents = "Hi there, welcome to index page"
+        return render(request,"home/home.html",{
+            "contents":contents
+        })
 
