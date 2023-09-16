0b3e32c0cf061c42979ac6ea1e6dc28ca0bd7845
mohsenbarari <mmchavoshi235@gmail.com>
2023-09-16 10:45:22 +0100

    add path root templetes dir in settings file

--- social_network/settings.py
+++ social_network/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'social_network.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
