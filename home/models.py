a118b68ad059930ee1fec45f9851abc55818ec86
mohsenbarari <mmchavoshi235@gmail.com.com>
2023-09-18 15:02:41 +0200

    bug fix

--- home/models.py
+++ home/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
-    title = models.TextField(max_length=50)
-    body = models.CharField(max_length=700)
+    title = models.CharField(max_length=50)
+    body = models.TextField(max_length=700)
     slug = models.SlugField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
