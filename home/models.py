7bef76b49c346c43d3dce79dc7edefa857be7564
mohsenbarari <mmchavoshi235@gmail.com.com>
2023-09-18 14:47:41 +0200

    add post model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.TextField(max_length=50)
+    body = models.CharField(max_length=700)
+    slug = models.SlugField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="posts")
