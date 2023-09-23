8d5cc1b6f80a254eaa615c30b59f29a3b17cfae3
mohsenbarari <mmchavoshi235@gmail.com.com>
2023-09-23 19:09:48 +0200

    add update post

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,9 @@ app_name = "home"
 urlpatterns = [
     path("index/",views.IndexView.as_view(),name="index"),
     path("deail/<int:post_id>/<slug:post_slug>/",views.PostDetailView.as_view(),name="post-detail"),
-    path("delete/<int:post_id>/",views.DeletePostView.as_view(),name="post-delete"),
+    path("delete/<int:post_id>/",views.PostDeleteView.as_view(),name="post-delete"),
+    path("update/<int:post_id>/",views.PostUpdateView.as_view(),name="post-update"),
+
 
     
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
+from typing import Any
+from django import http
 from django.shortcuts import render,redirect
 from django.views import View
 from .models import Post
 from django.contrib import messages
+from .forms import PostUpdateForm
+from django.utils.text import slugify
 
 
 class IndexView(View):
@@ -20,7 +24,7 @@ class PostDetailView(View):
         })
     
 
-class DeletePostView(View):
+class PostDeleteView(View):
 
     def get(self,request,post_id):
         post = Post.objects.get(pk=post_id)
@@ -30,3 +34,39 @@ class DeletePostView(View):
         else:
             messages.error(request,"you don't delete this post")
         return redirect('home:index')
+    
+
+class PostUpdateView(View):
+    form_class = PostUpdateForm
+    template_page = "home/update.html"
+
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = Post.objects.get(pk=kwargs["post_id"])
+        return super().setup(request, *args, **kwargs)
+    
+    def dispatch(self, request, *args, **kwargs):
+        post = self.post_instance
+        if post.user.id != request.user.id:
+            messages.error(request,"you don't permmsion this post","danger")
+            return redirect("home:post-detail",post.id,post.slug)
+        return super().dispatch(request, *args, **kwargs)
+    
+
+    def get(self,request,post_id):
+        post = self.post_instance
+        form = self.form_class(instance=post)
+        return render(request,self.template_page,{
+            "form":form,
+        })
+    def post(self,request,post_id):
+        post = self.post_instance
+        form = self.form_class(request.POST,instance=post)
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(cd["title"])
+            new_post.save()
+            messages.success(request,"update post is ok","success")
+            return redirect("home:post-detail" ,post.id, post.slug)
+
+
